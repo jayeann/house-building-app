68a3dd904bb4dac70f731356d2b2518d88e05afa
jayeann <jgtagoctoc@gmail.com>
2024-05-30 18:35:36 +0800

    add basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Homepage, BuildHouse, Summary, NotFound } from "./pages";
 import "./style/global.css";
 
 function App() {
   return (
     <Container className="App">
-      <Router>
+      <BrowserRouter basename={"/house-building-app/"}>
         <Routes>
           <Route index element={<Homepage />} />
           <Route path="/build" element={<BuildHouse />} />
           <Route path="/summary" element={<Summary />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </Container>
   );
 }
